0da5722774206af30159f52d9de817daefb279f4
fsantosandrade <160565775+fsantosandrade@users.noreply.github.com>
2024-05-04 15:32:57 -0300

    parte 2 aula sobre assincronismo e promises

--- 03_Conceitos_Avancados/04_promises_assincronismo/manipulando_com_promises.js
+++ 03_Conceitos_Avancados/04_promises_assincronismo/manipulando_com_promises.js
@@ -1,17 +1,43 @@
 const fs = require('fs')
 
-const testeDeLeitura = fs.promises.readFile('tarefasTeste.csv')
+// Usando os métodos then, catch e finaly
+// const testeDeLeitura = fs.promises.readFile('tarefasTeste.csv')
 
-testeDeLeitura
-    .then((arquivo) => arquivo.toString('utf-8'))
-    .then((texto) => texto.split('\n'))
-    .then((lista) => lista.map((linha) => {
-        const [nome, feito] = linha.split(';')
-        let batata = null
-        return batata = {
+// testeDeLeitura
+//     .then((arquivo) => arquivo.toString('utf-8'))
+//     .then((texto) => texto.split('\n'))
+//     .then((lista) => lista.map((linha) => {
+//         const [nome, feito] = linha.split(';')
+//         let batata = null
+//         return batata = {
+//             nome,
+//             feito
+//         }
+//     }))
+//     .then((result) => console.log(result))
+//     .catch((error) => console.error(error))]
+//     .finally(() => console.log('Finalizou!'))
+
+
+// Usando await e async
+
+async function buscarArquivo() {
+    try {const testeDeLeitura = await fs.promises.readFile('tarefasTeste.csv')
+    const textoDoArquivo = testeDeLeitura.toString('utf-8')
+    const textoEmLinhas = textoDoArquivo.split('\n')
+    const textoEmLista = textoEmLinhas.map((linhas) => {
+        const [nome, feito] = linhas.split(';')
+        return {
             nome,
             feito
         }
-    }))
-    .then((result) => console.log(result))
-    .catch((error) => console.error(error))
+    })
+    console.log(textoEmLista)
+    } catch (error) {
+        console.error(error)
+    } finally {
+        console.log('Finalizou!')
+    }
+}
+
+buscarArquivo()
